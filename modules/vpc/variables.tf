25ee456ff16113f9212a244a75a12cd240aba6bb
deepajaan <deepajaanu50@gmail.com>
2025-01-31 10:06:28 +0530

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,4 @@
+variable "vpc_cidr" {}
+# description = "(Optional) The IPv4 CIDR block for the VPC. CIDR can be explicitly set or it can be derived from IPAM using `ipv4_netmask_length` & `ipv4_ipam_pool_id`"
+# type        = string
+# default     = "10.0.0.0/16"
